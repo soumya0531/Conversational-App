2d298c675590848cd3eb5e48518383eef89411c2
Swastika <swassarangi18@gmail.com>
2020-11-17 13:58:17 +0530

    chat container created

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { createGlobalStyle } from "styled-components";
 
@@ -10,14 +10,31 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
-    background-color: #F7F3F5;
+    background-color: red;
+    padding: 10px 300px;
   }
   h1, h2, h3, h4, h5, h6 {
     font-weight: 700;
   }
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey; 
+    border-radius: 10px;
+  }
+   
+  ::-webkit-scrollbar-thumb {
+    background: #3F3250; 
+    border-radius: 10px;
+  }
+  
   main {
-    width: 60%;
+    width: 80%;
     padding: 6rem 0;
+    height: 500px;
+    
     margin: 0 auto;
     @media(max-width: 992px) {
       width: 80%;
@@ -28,22 +45,38 @@ const GlobalStyle = createGlobalStyle`
     width: 900px;
     margin: 100px auto;
     background-color: lightpink;
+    overflow-y:auto;
     
   }
   .chat-footer{
-    position: fixed;
-  bottom: 0;
-  width: 900px;
+    position: absolute;
+    bottom:0;
+    width: 900px;
+    margin: 0 auto;
   }
 `;
 
+
 const Layout = () => {
+  const ref = useRef(null);
+  const scrollToBottom = (direct = false) => {
+    // passing direct to this forces scrolling instantly, instead of animating.
+    if (ref.current?.scrollIntoView) {
+      ref.current.scrollIntoView({ behavior: direct ? "auto" : "smooth" });
+    }
+  };
+  useEffect(() => {
+    scrollToBottom();
+  });
   return (
     <Fragment>
       <GlobalStyle />
       <div className="chat">
+      <div className="chat-header">
       <ChatHeader />
-      <main><ChatContainer /></main>
+      </div>
+      <main><ChatContainer /><div
+        ref={ref} /></main>
       <div className="chat-footer">
       <ChatFooter />
       </div>
